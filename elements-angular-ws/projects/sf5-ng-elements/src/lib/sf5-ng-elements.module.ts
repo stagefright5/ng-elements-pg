d35b0e5405f718eaa6218146c1fe7e74d77f9ab9
stagefright5 <sankarshanaj@gmail.com>
2022-12-22 19:22:45 +0530

    feat: install @angular/elements

--- elements-angular-ws/projects/sf5-ng-elements/src/lib/sf5-ng-elements.module.ts
+++ elements-angular-ws/projects/sf5-ng-elements/src/lib/sf5-ng-elements.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Sf5NgElementsComponent } from './sf5-ng-elements.component';
-
-
+import { Sf5NgElementsComponent } from './components/sf5-ng-element/sf5-ng-elements.component';
 
 @NgModule({
-  declarations: [
-    Sf5NgElementsComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    Sf5NgElementsComponent
-  ]
+    declarations: [Sf5NgElementsComponent],
+    imports: [],
+    exports: [Sf5NgElementsComponent],
 })
-export class Sf5NgElementsModule { }
+export class Sf5NgElementsModule {
+  
+}
